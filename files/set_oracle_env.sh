aed876e6f4eae5c339ea0d3e569d0c43870f43e9
H.Y. Leung <hy.leung@gmail.com>
2013-04-16 19:54:34 -0700

    Fix for ORACLE_HOME export
    This commit fixes the profile.d script for setting ORACLE_HOME (missing
    'export').
    
    Also increases the swap space.

--- files/set_oracle_env.sh
+++ files/set_oracle_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -f /etc/oratab ] 
     then
-        ORACLE_HOME=$(cat /etc/oratab | grep  -o '[/.a-z0-9]*')
+        export ORACLE_HOME=$(cat /etc/oratab | grep  -o '[/.a-z0-9]*')
         PATH=$PATH:$ORACLE_HOME/bin
 fi
